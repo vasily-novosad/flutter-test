0f7f363319138dc9e5b9fd4c66457ed6b6a718b2
Vasily Novosad <73801977+vasily-novosad@users.noreply.github.com>
2025-02-03 16:03:54 +0500

    WIP

--- lib/services/graphql.dart
+++ lib/services/graphql.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test_app/services/storage_manager.dart';
 import 'package:http/http.dart' as http;
 import 'package:crypto/crypto.dart';
